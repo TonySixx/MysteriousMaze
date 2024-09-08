17c470eacc8891f7fbc909ec7cbbbddf506c79e9
TonySixx <75792327+TonySixx@users.noreply.github.com>
2024-09-08 19:50:21 +0200

    Delete assets directory

--- assets/browser-CD7b_Z6x.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./index-CMiyx2-u.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
