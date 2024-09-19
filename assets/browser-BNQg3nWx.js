d50aba0735d0e40f38fd4418640afc3298ca3699
TonySixx <75792327+TonySixx@users.noreply.github.com>
2024-09-19 21:26:42 +0200

    aaa

--- assets/browser-BNQg3nWx.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:assets/browser-BNQg3nWx.js
-import{g as e}from"./index-4AbvTM0c.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
-========
-import{g as e}from"./index-Cb4lTJhT.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
->>>>>>>> 2ada25e99111037d2b821f5d630789a8f7dee336:assets/browser-4huRJjAH.js
