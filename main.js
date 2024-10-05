94eb1b798bfc3be3fd7040f60dfbcbcf85e9df1b
TonySixx <75792327+TonySixx@users.noreply.github.com>
2024-10-05 18:00:08 +0200

    aa

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ import { toggleQuestWindow } from "./quests.js";
 import { clearScene } from "./clearScene.js";
 import { animate } from "./animate/mainAnimate.js";
 
-export const version = "3.1.5";
+export const version = "3.1.6";
 
 // Initialize Supabase client
 const supabaseUrl = "https://olhgutdozhdvniefmltx.supabase.co";
